b9b1f9325443e7d72b4620c9d98ff5ebe2b42071
viliamnovicky <viliamnovicky@gmail.com>
2025-02-24 19:47:16 +0100

    save coffee button

--- app/_components/NewCoffeeForm.js
+++ app/_components/NewCoffeeForm.js
@@ -3,6 +3,8 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { InfoParagraph, StatParagraph } from "./Paragraphs";
 import Tag from "./Tag";
 import { AddCoffeeButton } from "./Buttons";
+import { GiCoffeeBeans } from "react-icons/gi";
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -11,18 +13,18 @@ import { ImageInput, Input, Select, TagCheckbox } from "../_components/Inputs";
 
 function NewCoffeeForm() {
   const { coffee, resetNewCoffeeData, updateCoffeeData } = useNewCoffee();
-  
+
   function handleImageChange(file) {
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       updateCoffeeData("image", imageUrl);
     }
-    console.log(coffee.image)
-  };
+    console.log(coffee.image);
+  }
 
   return (
     <div className="relative w-[100vw] flex flex-col">
-      <AddCoffeeButton />
+      {/* <AddCoffeeButton /> */}
       <Link
         href="/coffees"
         className=" bg-primary-200 hover:bg-primary-300 w-[50px] h-[50px] flex rounded-full absolute left-3 top-3 items-center justify-center "
@@ -52,6 +54,10 @@ function NewCoffeeForm() {
         ></textarea>
       </form>
       <div className=" w-[100vw] h-auto grid grid-cols-1 md:grid-cols-[1fr_1fr_1fr_1fr] m-auto p-4">
+        <button className="bg-primary-400 hover:bg-primary-500 z-10 px-4 py-2 uppercase rounded-md absolute md:bottom-0 bottom-[50px] left-[50%] translate-x-[-50%] md:border-none border border-primary-50">
+          <GiCoffeeBeans className="z-0 text-[80px] text-primary-600 absolute left-[50%] bottom-0  translate-x-[-50%]" />
+          <span className="z-1 relative">save coffee</span>
+        </button>
         <div className="h-[550px] md:h-auto w-[100%] bg-gradient-1 rounded-t-full rounded-lt-none md:rounded-l-full md:rounded-r-none relative p-10 overflow-hidden flex flex-col justify-center items-center">
           {coffee.caffeine && (
             <Tag
@@ -67,7 +73,7 @@ function NewCoffeeForm() {
             height="400"
             className="object-cover rounded-full p-2 bg-primary-50"
           />
-          <ImageInput onImageSelect={handleImageChange} addClass="m-[-20px]"/>
+          <ImageInput onImageSelect={handleImageChange} addClass="m-[-20px]" />
         </div>
         <div className=" px-1 py-2 justify-start bg-gradient-2 h-auto w-[100%] flex flex-col">
           <InfoParagraph>
@@ -99,13 +105,13 @@ function NewCoffeeForm() {
           <InfoParagraph color="dark">
             type:
             <span className="flex gap-2 flex-wrap w-[70%] justify-end">
-              <TagCheckbox label="filter"/>
-              <TagCheckbox label="espresso"/>
-              <TagCheckbox label="frenchpress"/>
-              <TagCheckbox label="stovetop"/>
-              <TagCheckbox label="automat"/>
-              <TagCheckbox label="turkish"/>
-              </span>
+              <TagCheckbox label="filter" />
+              <TagCheckbox label="espresso" />
+              <TagCheckbox label="frenchpress" />
+              <TagCheckbox label="stovetop" />
+              <TagCheckbox label="automat" />
+              <TagCheckbox label="turkish" />
+            </span>
           </InfoParagraph>
         </div>
         <div
