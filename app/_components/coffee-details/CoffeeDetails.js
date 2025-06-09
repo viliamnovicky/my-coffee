5b1157674453aa7f49f99fc3ed963839ecbe7ee5
viliamnovicky <viliamnovicky@gmail.com>
2025-06-09 10:30:57 +0200

    fixed url on back button

--- app/_components/coffee-details/CoffeeDetails.js
+++ app/_components/coffee-details/CoffeeDetails.js
@@ -29,7 +29,7 @@ function CoffeeDetails({ coffee }) {
     <div className="relative w-screen flex flex-col xl:mt-[80px] mt-[140px]">
       <AddCoffeeButton />
       <Link
-        href="/my-coffees"
+        href="/coffees"
         className="z-40 bg-primary-200 hover:bg-primary-300 w-[35px] h-[35px] flex rounded-full fixed left-3 xl:top-[90px] top-[95px] items-center justify-center"
       >
         <IoMdArrowRoundBack className="text-[1.6rem]" />
