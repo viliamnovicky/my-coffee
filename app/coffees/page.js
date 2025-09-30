2937c41df9161b7725f4bc09571b2c4f510c6dfa
viliamnovicky <viliamnovicky@gmail.com>
2025-09-30 15:29:28 +0200

    fixed searchbar error in prod

--- app/coffees/page.js
+++ app/coffees/page.js
@@ -2,15 +2,17 @@ export const metadata = {
   title: "Coffee",
 };
 
+import { Suspense } from "react";
 import { AddCoffeeButton } from "../_components/Buttons";
 import MyAllCoffeesList from "../_components/MyAllCoffeesList";
 import Searchbar from "../_components/Searchbar";
 
 export async function Page() {
-
   return (
     <div className="relative">
-      <Searchbar user={false}/>
+      <Suspense>
+        <Searchbar user={false} />
+      </Suspense>
 
       <AddCoffeeButton />
 
